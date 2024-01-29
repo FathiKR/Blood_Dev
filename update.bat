a362ffa68df7e431a9c1af0d2837869ede6b65a6
fathikr <fathikr@gmail.com>
2024-01-29 13:39:02 +0800

    Update master data!

--- update.bat
+++ update.bat
@@ -7,9 +7,9 @@ cd /d C:\Work\Project\Dev\
 echo %CD%
 echo ### At source directory ###
 python master_data_processing.py
+python notification.py
 git pull
 git add .
 git commit -m "Update master data!"
 git push
 echo ####Push Telegram Notification######
-python notification.py
