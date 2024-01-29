a1b85e7b54244ed1a622b67e4af53983fa9daa85
fathikr <fathikr@gmail.com>
2024-01-29 13:59:00 +0800

    Update master data!

--- update.bat
+++ update.bat
@@ -2,14 +2,14 @@
 
 @echo off 
 set today=%Year%-%Month%-%Day%
-echo ### Script is running on: %today% ###
-cd /d C:\Work\Project\Dev\
+echo ### Script is running on: %today% ###\
+cd /d C:\Work\Project\Dev
 echo %CD%
 echo ### At source directory ###
 python master_data_processing.py
-python notification.py
 git pull
 git add .
 git commit -m "Update master data!"
 git push
 echo ####Push Telegram Notification######
+python notification.py
